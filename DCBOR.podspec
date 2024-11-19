a01d45c9da2a90ff2ca679e47add537243b1e856
soralit <soralitria@gmail.com>
2024-11-19 09:45:10 +0800

    chore: use apple official swift-collections

--- DCBOR.podspec
+++ DCBOR.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name         = "DCBOR"
-  spec.version      = "0.10.1"
+  spec.version      = "0.11.0"
   spec.summary      = "A pure Swift CBOR codec that focuses on writing and parsing deterministic CBOR per §4.2 of RFC-8949."
   spec.homepage     = "https://github.com/KeystoneHQ/BCSwiftDCBOR"
   spec.license      = { :type => "BSD", :file => "LICENSE" }
--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["DCBOR"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/wolfmcnally/swift-collections", .upToNextMajor(from: "1.1.0")),
+        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.1.0")),
         .package(url: "https://github.com/wolfmcnally/WolfBase.git", .upToNextMajor(from: "5.0.0")),
         .package(url: "https://github.com/KeystoneHQ/BCSwiftFloat16.git", .upToNextMajor(from: "0.3.0")),
     ],
